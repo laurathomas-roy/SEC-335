be0465a2bcfe730a51725b1a4d681fb1ea51db7a
Laura Thomas-Roy <143466475+laurathomas-roy@users.noreply.github.com>
2026-02-06 20:49:06 -0500

    week3: dns-resolver.ps1

---
+++ week3/dns-resolver.ps1
@@ -0,0 +1,12 @@
+﻿param($prefix, $server)
+
+for($i=0; $i -le 254; $i++) {
+    $network = "$prefix.$i"
+    $up = Resolve-DnsName -Name $network -Server $server -ErrorAction SilentlyContinue
+
+    foreach ($j in $up) {
+        if ($j.NameHost) {
+            Write-Host "$network $($j.NameHost)"
+        }
+    }
+}
